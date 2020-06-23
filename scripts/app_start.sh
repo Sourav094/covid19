4b16eb73c3f51ff7aff7d61064a4388cb05c18bc
Sourav094 <41063228+Sourav094@users.noreply.github.com>
2020-06-23 23:08:25 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /home/ubuntu/server
-rm -rf build
-cd src
-yarn build
+cd /home/ubuntu/server/src
+npm start
+pm2 start npm --name "covidapp" -- start
+pm2 startup
+pm2 save
+pm2 restart all
